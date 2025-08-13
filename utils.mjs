577ccaac9d1e0204d35dbb59f3c2ccfe9cf64da2
SharifKhaqan <sharifkhaqan5@gmail.com>
2025-08-13 18:21:56 +0500

    Practice Destructuring with Examples

---
+++ utils.mjs
@@ -0,0 +1,10 @@
+// Example2:
+export function validate(data) {
+  console.log("Validating:", data);
+}
+export function format(text) {
+  console.log("Formatting:", text);
+}
+export function sanitize(input) {
+  console.log("Sanitizing:", input);
+}
